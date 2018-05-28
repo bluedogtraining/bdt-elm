b34217b4ab54e080cc584cfa08c34bff0f8d0d88
ryan senn <ryan.senn.au@gmail.com>
2018-05-28 15:07:06 +1000

    Add Resettable module and tests

---
+++ src/Resettable.elm
@@ -0,0 +1,149 @@
+module Resettable exposing
+    ( Resettable
+    , init, update, reset
+    , isChanged, getValue, getOriginalValue
+    )
+
+
+{-| This module is useful if you want to track changes to a value.
+
+# Definition
+@docs Resettable
+
+# Initialise and update
+@docs init, update, reset
+
+# Getters
+@docs isChanged, getValue, getOriginalValue
+
+-}
+
+{-| Represent values that may be original or updated. It can be useful if you have a
+a save button that should be disabled if nothing on a form changed.
+
+    type alias MyProfileForm =
+        { firstName : Resettable String
+        , email : Resettable String
+        }
+
+    form =
+        { firstName = Resettable.init "John Doe"
+        , email = Resettable.init ""
+        }
+
+    shouldEnableSaveButton : Form -> Bool
+    shouldEnableSaveButton form =
+        Resettable.isChanged form.email || Resettable.isChanged form.password
+-}
+type Resettable a
+    = Original a
+    | Updated a a
+
+
+{-| Initialise a value.
+
+    init "Bob"     -- Original "Bob"
+-}
+init : a -> Resettable a
+init =
+    Original
+
+
+{-| Updates a Resettable value.
+
+    -- update to new value
+    update "Joshua" (Original "Josh")       -- Updated "Josh" "Joshua"
+    update "Bob" (Updated "Josh" "Joshua")  -- Updated "Josh" "Bob"
+
+    -- if the new value is the original value or a changed value is set to it's original value, keep the original value
+    update "Josh" (Original "Josh")         -- Original "Bob"
+    update "Josh" (Updated "Josh" "Joshua") -- Original "Josh"
+-}
+update : a -> Resettable a -> Resettable a
+update newValue resettable =
+
+    case resettable of
+        Original originalValue ->
+            case newValue == originalValue of
+                True ->
+                    resettable
+
+                False ->
+                    Updated originalValue newValue
+
+        Updated originalValue currentValue ->
+            case (currentValue == newValue, originalValue == newValue) of
+                (True, _) ->
+                    resettable
+
+                (False, True) ->
+                    Original originalValue
+
+                (False, False) ->
+                    Updated originalValue newValue
+
+
+{-| Whether a value changed.
+
+    isChanged (Original "Josh")         -- False
+    isChanged (Updated "Josh" "Joshua") -- True
+
+    when checking a lot of values, it may be helpful to put them in a list:
+
+    -- if they all have the same type:
+    List.any isChanged [ Original "Josh", Updated "Josh" "Joshua" ]   -- True
+
+    -- if they have different types
+    List.any ((==) True) [ isChanged (Original "Josh"), isChanged (Original 12) ]   -- False
+-}
+isChanged : Resettable a -> Bool
+isChanged resettable =
+
+    case resettable of
+        Original _ ->
+            False
+
+        Updated _ _ ->
+            True
+
+
+{-| Get the current value.
+
+    getValue (Original "Josh")         -- "Josh"
+    getValue (Updated "Josh" "Joshua") -- "Joshua"
+-}
+getValue : Resettable a -> a
+getValue resettable =
+
+    case resettable of
+        Original originalValue ->
+            originalValue
+
+        Updated _ updatedValue ->
+            updatedValue
+
+
+{-| Get the original value.
+
+    getValue (Original "Josh")         -- "Josh"
+    getValue (Updated "Josh" "Joshua") -- "Josh"
+-}
+getOriginalValue : Resettable a -> a
+getOriginalValue resettable =
+
+    case resettable of
+        Original originalValue ->
+            originalValue
+
+        Updated originalValue _ ->
+            originalValue
+
+
+{-| Reset to the original value.
+
+    reset (Original "Josh")         -- Original "Josh"
+    reset (Updated "Josh" "Joshua") -- Original "Josh"
+-}
+reset : Resettable a -> Resettable a
+reset =
+    getOriginalValue >> init
---
+++ tests/ResettableTests.elm
@@ -0,0 +1,77 @@
+module ResettableTests exposing (..)
+
+import Test exposing (..)
+import Fuzz exposing (Fuzzer)
+import Expect exposing (Expectation)
+
+import Resettable
+
+
+suite : Test
+suite =
+    describe "Test Getters with: "
+        [ describe "Fuzz.string" (tests Fuzz.string)
+        , describe "Fuzz.bool" (tests Fuzz.bool)
+        , describe "Fuzz.int" (tests Fuzz.int)
+        , describe "Fuzz.char" (tests Fuzz.char)
+        , describe "Fuzz.maybe" (tests (Fuzz.maybe Fuzz.string))
+        , describe "Fuzz.order" (tests Fuzz.order)
+        ]
+
+
+tests : Fuzzer a -> List Test
+tests fuzzer =
+    [ fuzz fuzzer "getValue on original value returns the original value." test1
+    , fuzz2 fuzzer fuzzer "getValue on updated value returns the updated value." test2
+    , fuzz fuzzer "getOriginalValue on original value returns the original value." test3
+    , fuzz2 fuzzer fuzzer "getOriginalValue on updated value returns the original value." test4
+    , fuzz2 fuzzer fuzzer "getValue after reset on updated value returns the original value." test5
+    , fuzz2 fuzzer fuzzer "isChanged on updated value returns true." test6
+    ]
+
+
+test1 : a -> Expectation
+test1 originalValue =
+    Resettable.init originalValue
+        |> Resettable.getValue
+        |> Expect.equal originalValue
+
+
+test2 : a -> a -> Expectation
+test2 originalValue updatedValue =
+    Resettable.init originalValue
+        |> Resettable.update updatedValue
+        |> Resettable.getValue
+        |> Expect.equal updatedValue
+
+
+test3 : a -> Expectation
+test3 originalValue =
+    Resettable.init originalValue
+        |> Resettable.getOriginalValue
+        |> Expect.equal originalValue
+
+
+test4 : a -> a -> Expectation
+test4 originalValue updatedValue =
+    Resettable.init originalValue
+        |> Resettable.update updatedValue
+        |> Resettable.getOriginalValue
+        |> Expect.equal originalValue
+
+
+test5 : a -> a -> Expectation
+test5 originalValue updatedValue =
+    Resettable.init originalValue
+        |> Resettable.update updatedValue
+        |> Resettable.reset
+        |> Resettable.getValue
+        |> Expect.equal originalValue
+
+
+test6 : a -> a -> Expectation
+test6 originalValue updatedValue =
+    Resettable.init originalValue
+        |> Resettable.update updatedValue
+        |> Resettable.isChanged
+        |> Expect.equal (originalValue /= updatedValue)

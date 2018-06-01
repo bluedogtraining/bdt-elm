c403e7b117c701eaefef0720739a57d60eb32437
ryan senn <ryan.senn.au@gmail.com>
2018-06-01 17:23:54 +1000

    start on SearchSelect

---
+++ src/Form/SearchSelect.elm
@@ -0,0 +1,257 @@
+module Form.SearchSelect exposing
+    ( Model, init
+    , Msg, update
+    , view, render
+    , reInitialise, reset
+    , setInitialOption, setSelectedOption, setIsOptionDisabled
+    , setIsError, setIsLocked, setIsClearable
+    , setDefaultLabel, setToLabel
+    , setId
+    , getIsChanged, getIsOpen
+    , getInitialOption, getSelectedOption
+    , getId
+    )
+
+{-| This module is useful if you want to add a Select Form element to your app.
+
+# Initialise and update
+@docs Model, init, Msg, update
+
+# View and render
+@docs view, render
+
+# State Setters
+@docs reInitialise, reset, setInitialOption, setSelectedOption, setIsOptionDisabled
+
+# View Setters
+@docs setIsError, setIsLocked, setIsClearable, setDefaultLabel, setToLabel, setId
+
+# Getters
+@docs getIsChanged, getIsOpen, getInitialOption, getSelectedOption, getId
+
+-}
+
+import Html.Styled exposing (Html)
+
+import Tuple
+
+import Form.Select.Internal as Internal
+
+
+{-| Add a Select.Model to your model.
+
+    type Title
+        = Mr
+        | Ms
+        | Dr
+
+    type alias MyModel =
+        { mySelect : Select.Model Title
+        }
+-}
+type Model option
+    = Model (Internal.State option)
+
+
+type View option
+    = View (Internal.State option) (Internal.ViewState option)
+
+
+{-| Add a Select.Model to your model.
+
+    myInitialModel : MyModel
+    myInitialModel =
+        { mySelect = Select.init [Mr, Ms, Dr]
+        }
+-}
+init : List option -> Model option
+init =
+    Internal.init >> Model
+
+
+{-| Add a Select.Msg to your Msg.
+
+    type MyMsg
+        = UpdateMySelect Select.Msg
+-}
+type alias Msg
+    = Internal.Msg
+
+
+{-| Use in your update function.
+
+    myUpdate : Msg -> Model -> (Model, Cmd Msg)
+    myUpdate msg model =
+        case msg of
+            UpdateMySelect selectMsg ->
+                let
+                    (newSelect, cmd) =
+                        Select.update selectMsg mode.mySelect
+                in
+                    { model | mySelect = newSelect } ! [ cmd ]
+-}
+update : Internal.Msg option -> Model option -> (Model option, Cmd (Internal.Msg option))
+update msg (Model state) =
+
+    Tuple.mapFirst Model (Internal.update msg state)
+
+
+{-| Transform an Select.Model into an Select.View, which allows us to pipe View Setters on it.
+
+    myView : Model -> Html Msg
+    myView model =
+        div
+            []
+            [ Select.view model.mySelect -- pipe view setters here, for example |> setIsLocked 'your logic here'
+            ]
+-}
+view : Model option -> View option
+view (Model state) =
+
+    View state Internal.initialViewState
+
+
+{-| Transforms an Select.View into Html Select.Msg
+
+    myView : Model -> Html Msg
+    myView model =
+        div
+            []
+            [ Select.view model.mySelect
+                |> Select.render
+                |> Html.map UpdateMySelect
+            ]
+-}
+render : View option -> Html (Internal.Msg option)
+render (View state viewState) =
+
+    Internal.render state viewState
+
+
+{-| ReInitialise your Select.Model.
+-}
+reInitialise : Model option -> Model option
+reInitialise (Model state) =
+
+    Model <| Internal.reInitialise state
+
+
+{-| Reset your Select.Model.
+-}
+reset : Model option -> Model option
+reset (Model state) =
+
+    Model <| Internal.reset state
+
+
+{-| Set the initial option of your Select.Model.
+-}
+setInitialOption : Maybe option -> Model option -> Model option
+setInitialOption selectedOption (Model state) =
+
+    Model <| Internal.setInitialOption selectedOption state
+
+
+{-| Change the option of your Select.Model.
+-}
+setSelectedOption : Maybe option -> Model option -> Model option
+setSelectedOption selectedOption (Model state) =
+
+    Model <| Internal.setSelectedOption selectedOption state
+
+
+{-| This function allows you to disable specific options.
+-}
+setIsOptionDisabled : (option -> Bool) -> View option -> View option
+setIsOptionDisabled isOptionDisabled (View state viewState) =
+
+    View state (Internal.setIsOptionDisabled isOptionDisabled viewState)
+
+
+{-| Set whether your select is locked (disabled).
+-}
+setIsLocked : Bool -> View option -> View option
+setIsLocked isLocked (View state viewState) =
+
+    View state (Internal.setIsLocked isLocked viewState)
+
+
+{-| Set whether your select is in error mode (red border).
+-}
+setIsError : Bool -> View option -> View option
+setIsError isError (View state viewState) =
+
+    View state (Internal.setIsError isError viewState)
+
+
+{-| Set whether your select is clearable (x icon).
+-}
+setIsClearable : Bool -> View option -> View option
+setIsClearable isClearable (View state viewState) =
+
+    View state (Internal.setIsClearable isClearable viewState)
+
+
+{-| Set how your options are printed to the screen.
+-}
+setToLabel : (option -> String) -> View option -> View option
+setToLabel toLabel (View state viewState) =
+
+    View state (Internal.setToLabel toLabel viewState)
+
+
+{-| Set the default label, for example (-- NOTHING SELECTED --).
+-}
+setDefaultLabel : String -> View option -> View option
+setDefaultLabel defaultLabel (View state viewState) =
+
+    View state (Internal.setDefaultLabel defaultLabel viewState)
+
+
+{-| Give your select an id. Can be useful for DOM selectors (focus, WebComponents etc.)
+-}
+setId : String -> View option -> View option
+setId id (View state viewState) =
+
+    View state (Internal.setId id viewState)
+
+
+{-| Whether your select was changed. Useful if you want to disable save buttons unless there were changes etc.
+-}
+getIsChanged : Model option -> Bool
+getIsChanged (Model state) =
+
+    Internal.getIsChanged state
+
+
+{-| Whether this select is currently open.
+-}
+getIsOpen : Model option -> Bool
+getIsOpen (Model state) =
+
+    Internal.getIsOpen state
+
+
+{-| Get the initial option of your select.
+-}
+getInitialOption : Model option -> Maybe option
+getInitialOption (Model state) =
+
+    Internal.getInitialOption state
+
+
+{-| Get the current option of your select. This is what you'd use to display the data somewhere outside of your select,
+or to send the data to the backend for example etc.
+-}
+getSelectedOption : Model option -> Maybe option
+getSelectedOption (Model state) =
+
+    Internal.getSelectedOption state
+
+
+{-| Useful if you need the id of the select in your update function, so set focus etc.
+-}
+getId : View option -> Maybe String
+getId (View _ viewState) =
+
+    Internal.getId viewState
---
+++ src/Form/SearchSelect/Internal.elm
@@ -0,0 +1,532 @@
+module Form.SearchSelect.Internal exposing
+    ( State, ViewState
+    , init, initialViewState
+    , Msg, update
+    , render
+    , reInitialise, reset
+    , setDefaultLabel, setToLabel
+    , setInitialOption, setSelectedOption, setIsOptionDisabled
+    , setIsError, setIsLocked, setIsClearable
+    , setId
+    , getIsChanged, getIsOpen
+    , getSelectedOption, getInitialOption
+    , getId
+    )
+
+import Html.Styled exposing (..)
+import Html.Styled.Lazy as Html
+import Html.Styled.Events exposing (..)
+import Html.Styled.Attributes as Attributes exposing (..)
+
+import Dom
+import VirtualDom
+import Dict
+import Task
+import Http
+
+import List.Extra as List
+
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline as Decode
+
+import Resettable exposing (Resettable)
+import Html.Bdt as Html exposing ((?))
+import Form.Helpers as Form
+
+import Form.SearchSelect.Css as Css
+
+
+-- MODEL --
+
+
+type alias State option =
+    { isOpen : Bool
+    , input : String
+    , searchUrl : String
+    , isSearching : Bool
+    , options : List option
+    , optionDecoder : Decoder option
+    , selectedOption : Resettable (Maybe option)
+    , focusedOption : Maybe option
+    }
+
+
+init : String -> Decoder option -> State option
+init searchUrl optionDecoder =
+    { isOpen = False
+    , input = ""
+    , searchUrl = searchUrl
+    , isSearching = False
+    , options = []
+    , optionDecoder = optionDecoder
+    , selectedOption = Resettable.init Nothing
+    , focusedOption = Nothing
+    }
+
+
+type alias ViewState option =
+    { inputMinimum : Int
+    , isLocked : Bool
+    , isClearable : Bool
+    , isError : Bool
+    , isOptionDisabled : option -> Bool
+    , toLabel : option -> String
+    , defaultLabel : String
+    , id : Maybe String
+    }
+
+
+initialViewState : ViewState option
+initialViewState =
+    { inputMinimum = 2
+    , isLocked = False
+    , isClearable = False
+    , isError = False
+    , isOptionDisabled = always False
+    , toLabel = toString
+    , defaultLabel = "-- Nothing Selected --"
+    , id = Nothing
+    }
+
+
+-- UPDATE --
+
+
+type Msg option
+    = Open
+    | Blur
+    | UpdateSearchInput Int String
+    | Response (Result Http.Error (SearchResponse option))
+    | SelectOption option
+    | Clear
+    | KeyboardInput KeyboardInput
+    | Focus option
+    | BlurOption option
+    | DomFocus (Result Dom.Error ())
+
+
+update : Msg option -> State option -> (State option, Cmd (Msg option))
+update msg state =
+
+    let
+        (newState, cmd) =
+            case msg of
+                Open ->
+                    { state | isOpen = True } ! [ Cmd.none ]
+
+                Blur ->
+                    { state | isOpen = False, input = "", focusedOption = Nothing } ! [ Cmd.none ]
+
+                UpdateSearchInput inputMinimum value ->
+                    let
+                        cmd =
+                            case shouldSearch inputMinimum value of
+                                True ->
+                                    searchRequest state.searchUrl value state.optionDecoder
+
+                                False ->
+                                    Cmd.none
+
+                    in
+                        { state | input = value, isSearching = shouldSearch inputMinimum value } ! [ cmd ]
+
+                Response result ->
+                    case result of
+                        Err error ->
+                            { state | isSearching = False } ! []
+
+                        Ok searchResponse ->
+                            { state | isSearching = False, options = searchResponse.options, focusedOption = Nothing } ! []
+
+                Clear ->
+                    { state | selectedOption = Resettable.update Nothing state.selectedOption } ! [ Cmd.none ]
+
+                SelectOption selectedOption ->
+                    { state
+                        | input = ""
+                        , selectedOption = Resettable.update (Just selectedOption) state.selectedOption
+                    } ! [ Cmd.none ]
+
+                KeyboardInput keyboardInput ->
+                    handleKeyboardInput state keyboardInput
+
+                Focus option ->
+                    { state | focusedOption = Just (focusOption state.options option) } ! []
+
+                BlurOption option ->
+                    if Just option == state.focusedOption then
+                        { state | focusedOption = Nothing, isOpen = False } ! []
+                    else
+                        state ! []
+
+                DomFocus _ ->
+                    state ! []
+
+    in
+        newState ! []
+
+
+type KeyboardInput
+    = Enter
+    | Space
+    | Up
+    | Down
+
+
+onKeyboardInput : (KeyboardInput -> msg) -> Attribute msg
+onKeyboardInput msg =
+
+    onWithOptions
+        "keydown"
+        { preventDefault = True, stopPropagation = True }
+        (Decode.andThen (isSelectInputKey msg) keyCode)
+
+
+isSelectInputKey : (KeyboardInput -> msg) -> Int -> Decoder msg
+isSelectInputKey msg code =
+
+    let
+        dict =
+            Dict.fromList [(13, Enter), (38, Up), (40, Down)]
+
+    in
+        case Dict.get code dict of
+
+            Just selectKeyboardInput ->
+                Decode.succeed (msg selectKeyboardInput)
+
+            Nothing ->
+                Decode.fail "Not a search-select input key"
+
+
+handleKeyboardInput: State option -> KeyboardInput -> (State option, Cmd (Msg option))
+handleKeyboardInput state keyboardInput =
+
+    case state.focusedOption of
+
+        Nothing ->
+            { state | focusedOption = List.head state.options } ! []
+
+        Just focusedOption ->
+            case keyboardInput of
+                Enter ->
+                    { state
+                        | selectedOption = Resettable.update (Just focusedOption) state.selectedOption
+                        , isOpen = False
+                    } ! []
+
+                Up ->
+                    let
+                        newFocusedOption =
+                            focusPreviousOption state.options focusedOption
+
+                    in
+                        { state | focusedOption = Just newFocusedOption }
+                            ! [ Task.attempt DomFocus (Form.toHtmlId newFocusedOption |> Dom.focus) ]
+
+                Down ->
+                    let
+                        newFocusedOption = focusNextOption state.options focusedOption
+
+                    in
+                        { state | focusedOption = Just newFocusedOption }
+                            ! [ Task.attempt DomFocus (Form.toHtmlId newFocusedOption |> Dom.focus) ]
+
+
+focusOption : List option -> option -> option
+focusOption options option =
+
+    case List.member option options of
+        True ->
+            option
+
+        False ->
+            Debug.crash ("MULTISELECT ERROR - can't focus" ++ Form.toHtmlId option ++ " it is not a valid option for this select.")
+
+
+focusPreviousOption : List option -> option -> option
+focusPreviousOption options option =
+
+    focusNextOption (List.reverse options) option
+
+
+focusNextOption : List option -> option -> option
+focusNextOption options option =
+
+    let
+        maybeNextOption =
+            options
+                |> List.dropWhile ((/=) option)
+                |> List.drop 1
+                |> List.head
+
+    in
+        case maybeNextOption of
+            Nothing ->
+                option
+
+            Just nextOption ->
+                nextOption
+
+
+-- SEARCH REQUEST --
+
+
+searchRequest : String -> String -> Decoder option -> Cmd (Msg option)
+searchRequest searchUrl input optionDecoder =
+    searchResponseDecoder optionDecoder
+        |> Http.get (searchUrl ++ "/" ++ input)
+        |> Http.send Response
+
+
+type alias SearchResponse option =
+    { options : List option
+    }
+
+
+searchResponseDecoder : Decoder option -> Decoder (SearchResponse option)
+searchResponseDecoder optionDecoder =
+    Decode.decode SearchResponse
+        |> Decode.required "options" (Decode.list optionDecoder)
+
+
+-- VIEW --
+
+
+render : State option -> ViewState option -> Html (Msg option)
+render state viewState =
+
+    case state.isOpen of
+        False ->
+            Html.lazy2 closed state viewState
+
+        True ->
+            Html.lazy2 open state viewState
+
+
+closed : State option -> ViewState option -> VirtualDom.Node (Msg option)
+closed state viewState =
+
+    div
+        [ Css.container ]
+        [ div
+            [ Css.input viewState.isLocked viewState.isError
+            , class "form-control"
+            , id (viewState.id |> Maybe.withDefault "")
+            , type_ "text"
+            , disabled viewState.isLocked
+            , tabindex 0 ? not viewState.isLocked
+            , onFocus Open ? not viewState.isLocked
+            , onClick Open ? not viewState.isLocked
+            , value (Maybe.map viewState.toLabel (Resettable.getValue state.selectedOption) |> Maybe.withDefault "")
+            ]
+            []
+        ]
+        |> Html.Styled.toUnstyled
+
+
+open : State option -> ViewState option -> VirtualDom.Node (Msg option)
+open state viewState =
+
+    div
+        [ Css.container ]
+        [ div
+            [ Css.input viewState.isLocked viewState.isError
+            , class "form-control"
+            , id (viewState.id |> Maybe.withDefault "")
+            , type_ "text"
+            , placeholder (Maybe.map viewState.toLabel (Resettable.getValue state.selectedOption) |> Maybe.withDefault "")
+            , tabindex -1
+            , disabled viewState.isLocked
+            , onInput (UpdateSearchInput viewState.inputMinimum)
+            , onBlur Blur
+            , onKeyboardInput KeyboardInput
+            , value (Maybe.map viewState.toLabel (Resettable.getValue state.selectedOption) |> Maybe.withDefault "")
+            ]
+            []
+        , searchResults state viewState
+        ]
+        |> Html.Styled.toUnstyled
+
+
+searchResults : State option -> ViewState option -> Html (Msg option)
+searchResults state viewState =
+
+    case shouldSearch viewState.inputMinimum state.input of
+        False ->
+            infoMessage (InputMinimum viewState.inputMinimum)
+
+        True ->
+            case state.isSearching of
+                True ->
+                    infoMessage Searching
+
+                False ->
+                    case List.isEmpty state.options of
+                        True ->
+                            infoMessage NoResults
+
+                        False ->
+                            searchResultList state viewState
+
+
+type InfoMessage
+    = InputMinimum Int
+    | Searching
+    | NoResults
+
+
+infoMessage : InfoMessage -> Html (Msg option)
+infoMessage message =
+
+    case message of
+        InputMinimum int ->
+            infoMessageContainer ("please type at least " ++ toString int ++ " characters to search")
+
+        Searching ->
+            infoMessageContainer "searching .."
+
+        NoResults ->
+            infoMessageContainer "no results"
+
+
+infoMessageContainer : String -> Html (Msg option)
+infoMessageContainer message =
+
+    div
+        [ Css.infoMessageContainer ]
+        [ text message ]
+
+
+searchResultList : State option -> ViewState option -> Html (Msg option)
+searchResultList state viewState =
+
+    div
+        [ Css.searchResultList ]
+        (List.map (searchResultItem state.focusedOption viewState.toLabel) state.options)
+
+
+searchResultItem : Maybe option -> (option -> String) -> option -> Html (Msg option)
+searchResultItem focusedOption toLabel option =
+
+    div
+        -- use onMouseDown over onClick so that it triggers before the onBlur on the input
+        [ Css.searchResultItem (Just option == focusedOption)
+        , id (Form.toHtmlId option)
+        , onMouseDown (SelectOption option)
+        , onFocus (Focus option)
+        , onBlur (BlurOption option)
+        , onKeyboardInput KeyboardInput
+        ]
+        [ text (toLabel option) ]
+
+
+-- STATE SETTERS --
+
+
+reInitialise : State option -> State option
+reInitialise state =
+
+    { state | selectedOption = Resettable.init (Resettable.getValue state.selectedOption) }
+
+
+reset : State option -> State option
+reset state =
+
+    { state | selectedOption = Resettable.reset state.selectedOption }
+
+
+setInitialOption : Maybe option -> State option -> State option
+setInitialOption selectedOption state =
+
+    { state | selectedOption = Resettable.init selectedOption }
+
+
+setSelectedOption : Maybe option -> State option -> State option
+setSelectedOption selectedOption state =
+
+    { state | selectedOption = Resettable.update selectedOption state.selectedOption }
+
+
+-- VIEW STATE SETTERS --
+
+
+setToLabel : (option -> String) -> ViewState option -> ViewState option
+setToLabel toLabel viewState =
+
+    { viewState | toLabel = toLabel }
+
+
+setDefaultLabel : String -> ViewState option -> ViewState option
+setDefaultLabel defaultLabel viewState =
+
+    { viewState | defaultLabel = defaultLabel }
+
+
+setIsOptionDisabled : (option -> Bool) -> ViewState option -> ViewState option
+setIsOptionDisabled isOptionDisabled viewState =
+
+    { viewState | isOptionDisabled = isOptionDisabled }
+
+
+setIsLocked : Bool -> ViewState option -> ViewState option
+setIsLocked isLocked viewState =
+
+    { viewState | isLocked = isLocked }
+
+
+setIsError : Bool -> ViewState option -> ViewState option
+setIsError isError viewState =
+
+    { viewState | isError = isError }
+
+
+setIsClearable : Bool -> ViewState option -> ViewState option
+setIsClearable isClearable viewState =
+
+    { viewState | isClearable = isClearable }
+
+
+setId : String -> ViewState option -> ViewState option
+setId id viewState =
+
+    { viewState | id = Just id }
+
+
+-- GETTERS --
+
+
+getIsChanged : State option -> Bool
+getIsChanged state =
+
+    Resettable.getIsChanged state.selectedOption
+
+
+getInitialOption : State option -> Maybe option
+getInitialOption state =
+
+    Resettable.getInitialValue state.selectedOption
+
+
+getSelectedOption : State option -> Maybe option
+getSelectedOption state =
+
+    Resettable.getValue state.selectedOption
+
+
+getIsOpen : State option -> Bool
+getIsOpen =
+    .isOpen
+
+
+getId : ViewState option -> Maybe String
+getId =
+    .id
+
+
+-- HELPERS --
+
+
+shouldSearch : Int -> String -> Bool
+shouldSearch inputMinimum input =
+    String.length input >= inputMinimum

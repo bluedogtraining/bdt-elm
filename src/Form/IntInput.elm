7ca6f2ba6dd699b5e2c8daa2c1bee613f64f7c6b
JoshSmithCode <necrosmium@gmail.com>
2018-06-05 10:57:46 +1000

    add IntInput

---
+++ src/Form/IntInput.elm
@@ -0,0 +1,222 @@
+module Form.IntInput exposing
+    ( Model, init
+    , Msg, update
+    , view, render
+    , reInitialise, reset
+    , setInitialValue, setValue
+    , setPlaceholder, setMaxLength
+    , setIsError, setIsLocked
+    , setId
+    , getIsChanged, getInitialValue, getValue
+    , getId
+    )
+
+
+{-| This module is useful if you want to add an Integer based Input element to your app.
+
+# Initialise and update
+@docs Model, init, Msg, update
+
+# View and render
+@docs view, render
+
+# State Setters
+@docs reInitialise, reset, setInitialValue, setValue
+
+# View Setters
+@docs setPlaceholder, setMaxLength, setIsError, setIsLocked, setId
+
+# Getters
+@docs getInitialValue, getValue, getIsChanged, getId
+
+-}
+
+import Html.Styled exposing (Html)
+
+import Form.IntInput.Internal as Internal
+
+
+{-| Add a Input.Model to your model.
+
+    type alias MyModel =
+        { myInput : Input.Model
+        }
+-}
+type Model
+    = Model Internal.State
+
+
+type View
+    = View Internal.State Internal.ViewState
+
+
+{-| Add a Input.Model to your model.
+
+    myInitialModel : MyModel
+    myInitialModel =
+        { myInput = Input.init -- optionally pipe into State Setters
+        }
+-}
+init : Model
+init =
+    Model Internal.init
+
+
+{-| Add a Input.Msg to your Msg.
+
+    type MyMsg
+        = UpdateMyInput Input.Msg
+-}
+type alias Msg
+    = Internal.Msg
+
+
+{-| Use in your update function.
+
+    myUpdate : Msg -> Model -> (Model, Cmd Msg)
+    myUpdate msg model =
+        case msg of
+            UpdateMyInput inputMsg ->
+                { model | myInput = Input.update inputMsg mode.myInput } ! []
+-}
+update : Internal.Msg -> Model -> Model
+update msg (Model state) =
+
+    Model (Internal.update msg state)
+
+
+{-| Transform an Input.Model into an Input.View, which allows us to pipe View Setters on it.
+
+    myView : Model -> Html Msg
+    myView model =
+        div
+            []
+            [ Input.view model.myInput -- pipe view setters here, for example |> setIsLocked 'your logic here'
+            ]
+-}
+view : Model -> View
+view (Model state) =
+
+    View state Internal.initialViewState
+
+
+{-| Transforms an Input.View into Html Input.Msg
+
+    myView : Model -> Html Msg
+    myView model =
+        div
+            []
+            [ Input.view model.myInput
+                |> Input.render
+                |> Html.map UpdateMyInput
+            ]
+-}
+render : View -> Html Internal.Msg
+render (View state viewState) =
+
+    Internal.render state viewState
+
+
+{-| ReInitialise your Input.Model.
+-}
+reInitialise : Model -> Model
+reInitialise (Model state) =
+
+    Model <| Internal.reInitialise state
+
+
+{-| Reset your Input.Model.
+-}
+reset : Model -> Model
+reset (Model state) =
+
+    Model <| Internal.reset state
+
+
+{-| Set the initial value of your Input.Model.
+-}
+setInitialValue : Int -> Model -> Model
+setInitialValue value (Model state) =
+
+    Model <| Internal.setInitialValue value state
+
+
+{-| Change the value of your Input.Model.
+-}
+setValue : Int -> Model -> Model
+setValue value (Model state) =
+
+    Model <| Internal.setValue value state
+
+
+{-| Set the max length for your input string.
+-}
+setMaxLength : Int -> View -> View
+setMaxLength maxLength (View state viewState) =
+
+    View state (Internal.setMaxLength maxLength viewState)
+
+
+{-| Set a placeholder for your input.
+-}
+setPlaceholder : String -> View -> View
+setPlaceholder placeholder (View state viewState) =
+
+    View state (Internal.setPlaceholder placeholder viewState)
+
+
+{-| Set whether your input is locked (disabled).
+-}
+setIsLocked : Bool -> View -> View
+setIsLocked isLocked (View state viewState) =
+
+    View state (Internal.setIsLocked isLocked viewState)
+
+
+{-| Set whether your input is in error mode (red border).
+-}
+setIsError : Bool -> View -> View
+setIsError isError (View state viewState) =
+
+    View state (Internal.setIsError isError viewState)
+
+
+{-| Give your input an id. Can be useful for DOM selectors (focus, WebComponents etc.)
+-}
+setId : String -> View -> View
+setId id (View state viewState) =
+
+    View state (Internal.setId id viewState)
+
+
+{-| Whether your input was changed. Useful if you want to disable save buttons unless there were changes etc.
+-}
+getIsChanged : Model -> Bool
+getIsChanged (Model state) =
+
+    Internal.getIsChanged state
+
+
+{-| Get the initial value of your input.
+-}
+getInitialValue : Model -> Maybe Int
+getInitialValue (Model state) =
+
+    Internal.getInitialValue state
+
+
+{-| Get the current value of your input. This is what you'd use to display the data somewhere outside of your input,
+or to send the data to the backend for example etc.
+-}
+getValue : Model -> Maybe Int
+getValue (Model state) =
+
+    Internal.getValue state
+
+
+{-| Useful if you need the id of the input in your update function, so set focus etc.
+-}
+getId : View -> Maybe String
+getId (View _ viewState) =
+
+    Internal.getId viewState
---
+++ src/Form/IntInput/Internal.elm
@@ -0,0 +1,206 @@
+module Form.IntInput.Internal exposing
+    ( State, ViewState
+    , init, initialViewState
+    , Msg, update
+    , render
+    , reInitialise, reset
+    , setInitialValue, setValue
+    , setPlaceholder, setMaxLength
+    , setIsError, setIsLocked
+    , setId
+    , getIsChanged, getInitialValue, getValue
+    , getId
+    )
+
+import Html.Styled exposing (..)
+import Html.Styled.Lazy as Html
+import Html.Styled.Events exposing (..)
+import Html.Styled.Attributes exposing (..)
+
+import Html.Bdt as Html exposing ((?))
+
+import VirtualDom
+import Regex exposing (Regex)
+
+import Resettable exposing (Resettable)
+
+import Form.Input.Css as Css
+
+
+type alias State =
+    { value : Resettable String
+    }
+
+
+init : State
+init =
+    { value = Resettable.init ""
+    }
+
+
+type alias ViewState =
+    { maxLength : Maybe Int
+    , placeholder : String
+    , isLocked : Bool
+    , isError : Bool
+    , id : Maybe String
+    }
+
+
+initialViewState : ViewState
+initialViewState =
+    { maxLength = Nothing
+    , placeholder = ""
+    , isLocked = False
+    , isError = False
+    , id = Nothing
+    }
+
+
+-- UPDATE --
+
+
+type Msg
+    = Input String
+
+
+update : Msg -> State -> State
+update (Input string) state =
+
+    case Regex.contains (Regex.regex "^[-]?[0-9]*$") string of
+
+        True ->
+            { state | value = Resettable.update string state.value }
+
+        False ->
+            state
+
+
+-- VIEW --
+
+
+render : State -> ViewState -> Html Msg
+render state viewState =
+
+    Html.lazy2 inputField state viewState
+
+
+inputField : State -> ViewState -> VirtualDom.Node Msg
+inputField state viewState =
+
+    input
+        [ Css.inputField viewState.isLocked viewState.isError
+        , class "form-control"
+        , disabled viewState.isLocked
+        , value <| Resettable.getValue state.value
+        , onInput Input
+        , placeholder viewState.placeholder
+        , Html.maybeAttribute maxlength viewState.maxLength
+        ]
+        []
+        |> Html.Styled.toUnstyled
+
+
+-- STATE SETTERS --
+
+
+reInitialise : State -> State
+reInitialise state =
+
+    { state | value = Resettable.init <| Resettable.getValue state.value }
+
+
+reset : State -> State
+reset state =
+
+    { state | value = Resettable.reset state.value }
+
+
+setInitialValue : Int -> State -> State
+setInitialValue value state =
+
+    { state | value = Resettable.init (toString value) }
+
+
+setValue : Int -> State -> State
+setValue value state =
+
+    { state | value = Resettable.update (toString value) state.value }
+
+
+-- VIEW STATE SETTERS --
+
+
+setMaxLength : Int -> ViewState -> ViewState
+setMaxLength maxLength viewState =
+
+    { viewState | maxLength = Just maxLength }
+
+
+setPlaceholder : String -> ViewState -> ViewState
+setPlaceholder placeholder viewState =
+
+    { viewState | placeholder = placeholder }
+
+
+setIsLocked : Bool -> ViewState -> ViewState
+setIsLocked isLocked viewState =
+
+    { viewState | isLocked = isLocked }
+
+
+setIsError : Bool -> ViewState -> ViewState
+setIsError isError viewState =
+
+    { viewState | isError = isError }
+
+
+setId : String -> ViewState -> ViewState
+setId id viewState =
+
+    { viewState | id = Just id }
+
+
+-- GETTERS --
+
+
+getIsChanged : State -> Bool
+getIsChanged state =
+
+    Resettable.getIsChanged state.value
+
+
+getInitialValue : State -> Maybe Int
+getInitialValue =
+
+    .value >> Resettable.getInitialValue >> stringToMaybeInt
+
+
+getValue : State -> Maybe Int
+getValue =
+
+    .value >> Resettable.getValue >> stringToMaybeInt
+
+
+stringToMaybeInt : String -> Maybe Int
+stringToMaybeInt string =
+
+    case string of
+
+        "" ->
+            Nothing
+
+        value ->
+            -- Don't use withDefault or mapError here, Debug.crash STILL gets called even if successful (compiler bug)
+            case String.toInt value of
+
+                Err _ ->
+                    Debug.crash "Failed to parse IntInput String to Int"
+
+                Ok int ->
+                    Just int
+
+
+getId : ViewState -> Maybe String
+getId =
+    .id

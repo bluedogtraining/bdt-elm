bef6999ad8dd367e9bc7423eaae1f690420dea18
ryan senn <ryan.senn.au@gmail.com>
2018-07-09 17:52:49 +1000

    add Date Helpers

---
+++ src/Date/Bdt.elm
@@ -0,0 +1,128 @@
+module Date.Bdt exposing
+    ( toString, maybeDateToString, maybeDateToTimeString, maybeDateToDateTimeString, monthToString
+    , order
+    )
+
+{-| Date Helpers
+
+# Print Dates
+@docs toString, maybeDateToString, maybeDateToTimeString, maybeDateToDateTimeString, monthToString
+
+# Sort Dates
+@docs order
+
+-}
+
+import Date exposing (Date, Month (..), month)
+import Date.Extra.Compare
+import Date.Extra.Format exposing (isoString)
+
+
+{-| Returns a string as dd/mm/yyyy
+-}
+toString : Date -> String
+toString date =
+
+    let
+        day =
+            date
+                |> Date.day
+                |> Basics.toString
+                |> String.pad 2 '0'
+
+        month =
+            date
+                |> Date.month
+                |> monthToString
+
+        year =
+            date
+                |> Date.year
+                |> Basics.toString
+
+    in
+        [ day, month, year ]
+            |> String.join "/"
+
+
+{-| Returns a string as `dd/mm/yyyy`, defaulted to `––/––/––––`
+-}
+maybeDateToString : Maybe Date -> String
+maybeDateToString date =
+
+    date
+        |> Maybe.map toString
+        |> Maybe.withDefault "––/––/––––"
+
+
+{-| Returns a string as `ss:mm:hh`, defaulted to `00:00:00`
+-}
+maybeDateToTimeString : Maybe Date -> String
+maybeDateToTimeString date =
+
+    case date of
+
+            Nothing ->
+                "00:00:00"
+
+            Just date ->
+                [ Date.hour, Date.minute, Date.second ]
+                    |> List.map (((|>) date) >> Basics.toString >> String.pad 2 '0')
+                    |> List.intersperse ":"
+                    |> String.concat
+
+
+{-| Returns a string as `ss:mm:hh dd/mm/yyyy`, defaulted to `00:00:00 ––/––/––––`
+-}
+maybeDateToDateTimeString : Maybe Date -> String
+maybeDateToDateTimeString date =
+
+    maybeDateToString date ++ " " ++ maybeDateToTimeString date
+
+
+{-| Returns a padded Int representation of the month
+
+    Jan -> "01"
+    Feb -> "02"
+    Mar -> "03"
+    Apr -> "04"
+    May -> "05"
+    Jun -> "06"
+    Jul -> "07"
+    Aug -> "08"
+    Sep -> "09"
+    Oct -> "10"
+    Nov -> "11"
+    Dec -> "12"
+-}
+monthToString : Month -> String
+monthToString month =
+
+    case month of
+        Jan -> "01"
+        Feb -> "02"
+        Mar -> "03"
+        Apr -> "04"
+        May -> "05"
+        Jun -> "06"
+        Jul -> "07"
+        Aug -> "08"
+        Sep -> "09"
+        Oct -> "10"
+        Nov -> "11"
+        Dec -> "12"
+
+
+{-| Orders 2 dates. This comes in handy with List.sortWith:
+
+    List.sortWith Date.order [date1, date2, date3]
+-}
+order : Date -> Date -> Order
+order date1 date2 =
+
+    if Date.Extra.Compare.is Date.Extra.Compare.Before date1 date2 then
+        LT
+    else if Date.Extra.Compare.is Date.Extra.Compare.After date1 date2 then
+        GT
+    else
+        EQ
---
+++ tests/DateTests.elm
@@ -0,0 +1,81 @@
+module DateTests exposing (suite)
+
+import Test exposing (..)
+import Expect exposing (Expectation)
+
+import Time
+
+import Date exposing (Date)
+import Date.Bdt as Date
+
+
+suite : Test
+suite =
+    describe "Test Date"
+        [ test "test toString" test1
+        , test "test maybeDateToString with Just" test2
+        , test "test maybeDateToString with Nothing" test3
+        , test "test maybeDateToTimeString with Just" test4
+        , test "test maybeDateToTimeString with Nothing" test5
+        , test "test maybeDateToDateTimeString with Just" test6
+        , test "test maybeDateToDateTimeString with Nothing" test7
+        , test "test order" test8
+        ]
+
+
+test1 : () -> Expectation
+test1 _ =
+    Date.toString (Date.fromTime <| Time.second * 1531122187)
+        |> Expect.equal "09/07/2018"
+
+
+test2 : () -> Expectation
+test2 _ =
+    Date.maybeDateToString (Just <| Date.fromTime <| Time.second * 1531122187)
+        |> Expect.equal "09/07/2018"
+
+
+test3 : () -> Expectation
+test3 _ =
+    Date.maybeDateToString Nothing
+        |> Expect.equal "––/––/––––"
+
+
+test4 : () -> Expectation
+test4 _ =
+    Date.maybeDateToTimeString (Just <| Date.fromTime <| Time.second * 1531122187)
+        |> Expect.equal "17:43:07"
+
+
+test5 : () -> Expectation
+test5 _ =
+    Date.maybeDateToTimeString Nothing
+        |> Expect.equal "00:00:00"
+
+
+test6 : () -> Expectation
+test6 _ =
+    Date.maybeDateToDateTimeString (Just <| Date.fromTime <| Time.second * 1531122187)
+        |> Expect.equal "09/07/2018 17:43:07"
+
+
+test7 : () -> Expectation
+test7 _ =
+    Date.maybeDateToDateTimeString Nothing
+        |> Expect.equal "––/––/–––– 00:00:00"
+
+
+test8 : () -> Expectation
+test8 _ =
+    [ Date.fromTime <| Time.second * 1531122187
+    , Date.fromTime <| Time.second * 1431022187
+    , Date.fromTime <| Time.second * 1631522187
+    , Date.fromTime <| Time.second * 1501522187
+    ]
+        |> List.sortWith Date.order
+        |> Expect.equal
+            [ Date.fromTime <| Time.second * 1431022187
+            , Date.fromTime <| Time.second * 1501522187
+            , Date.fromTime <| Time.second * 1531122187
+            , Date.fromTime <| Time.second * 1631522187
+            ]

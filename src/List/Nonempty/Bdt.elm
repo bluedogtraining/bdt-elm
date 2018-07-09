561c4868ec5a842156f62e60d853c21358eb9a5d
TheDuffman <rohman255@gmail.com>
2018-07-09 16:41:55 +1000

    add nonempty helpers

--- src/List/Nonempty/Bdt.elm
+++ src/List/Nonempty/Bdt.elm
@@ -1,11 +1,33 @@
 module List.Nonempty.Bdt exposing (decoder)
 
+{-| Nonempty Helpers
+
+# Decode a Nonempty
+@docs decoder
+
+-}
+
 import Json.Decode as Decode exposing (Decoder)
 import Json.Decode.Extra as Decode
 
 import List.Nonempty as Nonempty exposing (Nonempty)
 
 
+{-| Nonempty decoder
+
+type alias Student =
+    { firstName : String
+    , courses : Nonempty Course
+    }
+
+
+decoder : Decoder Student
+decoder =
+    Decode.decode Student
+        |> Decode.required "firstName" Decode.string
+        |> Decode.required "courses" (Nonempty.decoder courseDecoder)
+
+-}
 decoder : Decoder a -> Decoder (Nonempty a)
 decoder decoder =
 

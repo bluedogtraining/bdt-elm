0c6ca79b458dd5969df27cfeb1e5beb0bd665fb7
ryan senn <ryan.senn.au@gmail.com>
2018-09-12 11:18:45 +1000

    add elm docs

--- src/Set/Any/Bdt.elm
+++ src/Set/Any/Bdt.elm
@@ -1,8 +1,17 @@
 module Set.Any.Bdt exposing (toggle)
 
+{-| Set.Any Helpers
+
+# Helpers
+@docs toggle
+
+-}
+
 import Set.Any as AnySet exposing (AnySet)
 
 
+{-| Toggle an item (add if not existing, remove if existing)
+-}
 toggle : a -> AnySet comparable a -> AnySet comparable a
 toggle item set =
     if AnySet.member item set then

885cb9992bc78a543b42bed9626ea91ebce23a58
ryan senn <ryan.senn.au@gmail.com>
2018-11-06 17:51:11 +1000

    capitalise Area

--- src/Form/TextArea/Css.elm
+++ src/Form/TextArea/Css.elm
@@ -1,4 +1,4 @@
-module Form.Textarea.Css exposing (input)
+module Form.TextArea.Css exposing (input)
 
 import Css exposing (..)
 import Form.Css as Css
@@ -6,13 +6,11 @@ import Html.Styled exposing (Attribute)
 import Html.Styled.Attributes exposing (css)
 
 
-
 input : Bool -> Bool -> Attribute msg
 input isError isLocked =
     css <|
         Css.select isError isLocked
-        ++
-        [ flexGrow <| int 1
-        , padding <| rem 0.4
-        , cursor text_
-        ]
+            ++ [ flexGrow <| int 1
+               , padding <| rem 0.4
+               , cursor text_
+               ]
